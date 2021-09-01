088adbddfbacb8e1d5d85369294b2d4bf12926a6
Saurabh <saurabhdesale007@gmail.com>
2021-09-01 12:16:11 +0530

    Enter a message

---
+++ com/linkedlist/rohan/LinkedList.java
@@ -0,0 +1,7 @@
+package com.linkedlist.rohan;
+
+public class LinkedList {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Data Structure using LinkedList,Stack and Queues ");
+	}	
+}
